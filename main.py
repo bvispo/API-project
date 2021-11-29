be35f3773bb10d99cb61fc66c66fe2bb4f1621b9
Blanca Vispo <b.vispo@hotmail.com>
2021-11-29 18:23:52 +0100

    sentiment analysis done

--- main.py
+++ main.py
@@ -2,34 +2,53 @@ from os import name
 from flask import Flask, request, jsonify
 from sqlalchemy.sql.expression import text
 import tools.main_tools as mt
-import random
 from tools.main_tools import personajes
 
 
 app = Flask(__name__)
 
+# START
 @app.route("/")
 def inicio():
+    """
+    This function initializes the API.
+    """
     return "The office"
 
+# ENDPOINTS GET:
 
 @app.route("/personajes")
 def speakers():
+    """
+    This function gets all the characters.
+    """
     persons = mt.personajes()
     return jsonify(persons)
 
 @app.route("/frases/<name>")
 def quotes_names(name):
+    """
+    This function gets a random quote from a given character.
+    """
     frases = f"{name} says {mt.quotes(name)[0]}"
     return jsonify(frases)
 
 @app.route("/frase/<episodio>")
 def quotes_episode(episodio):
+    """
+    This function gets a random quote from a given episode.
+    """
     frases = mt.episodios(episodio)
     return jsonify(frases)
 
+
+# ENDPOINTS POST:
+
 @app.route("/nuevafrase", methods=["POST"])
 def insertamensaje():
+    """
+    This funcion posts a new quote for an already existen character.
+    """
     episodio = request.form.get("episode")
     personaje = request.form.get("speaker")
     texto = request.form.get("text")
@@ -38,21 +57,24 @@ def insertamensaje():
 
     return mt.nuevomensaje(episodio,personaje,texto)
 
-@app.route("/borrafrase",  methods=["POST"])
+@app.route("/borrafrase", methods=["POST"])
 def borrar():
-    return mt.borramensaje(text)
+    """
+    This funcion delates a quote from the sql database.
+    """
+    texto = request.form.get("text")
+    return mt.borramensaje(texto)
 
 
+# ENDPOINTS SENTIMENTAL ANALYSIS:
 
-
-
-
-
-
-
-
-
-
+@app.route("/sentimientos/<name>")
+def sentimientos(name):
+    df= mt.analisis_sentimientos(name)
+    df["phrases_token"] = df["text"].apply(mt.tokenizer)
+    df["resultado"] = df["phrases_token"].apply(mt.sentiment)
+    print (df.resultado)
+    return str(df.resultado.mean())
 
 
 app.run(debug=True)
--- tools/main_tools.py
+++ tools/main_tools.py
@@ -1,18 +1,39 @@
 from config.configuration import engine
 import pandas as pd
 import random
+import spacy
+import en_core_web_sm
+from nltk.corpus import stopwords
+import re
+from textblob import TextBlob
+import nltk
+nltk.downloader.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+# ENDPOINTS GET:
 
 def personajes():
+    """
+    This funcion returns a list with the characters names.
+    """
     query = list(engine.execute("SELECT distinct(speaker_name) FROM api.speaker;"))
     lista =  [{"nombre": elemento[0]} for elemento in query]
     return lista
 
+
 def quotes(personaje):
+    """
+    This funcion returns a random quote of a given characters.
+    """
     person = list(engine.execute(f"SELECT idspeaker FROM api.speaker WHERE speaker_name = '{personaje}' ;"))[0][0]
     what = list(engine.execute(f"SELECT text FROM text WHERE speaker_idspeaker = '{person}';"))
-    return what
+    return random.choices(what)
+
 
 def episodios(episodio):
+    """
+    This funcion returns a quote of a given episode.
+    """
     frase = random.choice(list(engine.execute(f"""
     SELECT text 
     FROM text
@@ -24,7 +45,13 @@ def episodios(episodio):
     return {f"La frase del capítulo {episodio}": frase}
 
 
+# ENDPOINTS POST:
+
 def nuevomensaje(episode,speaker,text):
+    """
+    This funcion posts a new quote for an already existen character.
+    Arggs: a dictionaty with the required values: episode, speaker, text.
+    """
     engine.execute(f"""
     INSERT INTO text (episode_idepisode, speaker_idspeaker,text)
     VALUES ({episode}, {speaker}, '{text}');
@@ -34,6 +61,10 @@ def nuevomensaje(episode,speaker,text):
 
 
 def borramensaje(text):
+    """
+    This funcion delates a quote from the sql database.
+    Arggs: a variable with the quote to be delated.
+    """
     engine.execute(f"""
     SELECT text FROM api.text WHERE text= '{text}';""")
     
@@ -42,3 +73,48 @@ def borramensaje(text):
 
     return "The text has been deleted"
 
+
+# ENDPOINTS SENTIMENTAL ANALYSIS:
+
+def analisis_sentimientos(speaker):
+    """
+    This gets all quotes from a given character.
+    Arggs: the speaker name.
+    """
+
+    query = f"""
+    SELECT text.text
+    FROM api.text
+    INNER JOIN api.speaker
+    ON speaker.idspeaker = text.speaker_idspeaker
+    WHERE speaker_name = '{speaker}';
+    """
+    data = pd.read_sql_query(query, engine)
+    return data
+
+
+def tokenizer(texto):
+    """
+    This function recieves a df and takes out from it the stopwords in english. 
+    It also replaces the suffixes of the words to leave them at the root.
+    Arggs: data frame
+    Returns: string with the root of the given words.    
+    """
+
+    nlp = spacy.load("en_core_web_sm")
+    stop = nlp.Defaults.stop_words
+    tokens = nlp(texto)
+    filter = []
+    for i in tokens:
+        if not i.is_stop:
+            lemma = i.lemma_.lower().strip()
+            if re.search('^[a-zA-Z]+$',lemma):
+                filter.append(lemma)
+    return " ".join(filter)
+
+
+def sentiment(phrases):
+    sia = SentimentIntensityAnalyzer()
+    polaridad = sia.polarity_scores(phrases)
+    pol = polaridad["compound"]
+    return pol
